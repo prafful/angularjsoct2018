4651ac25e85e121e9e9004e1eb553523b533d045
prafful <prraful@gmail.com>
2018-10-31 17:56:58 +0530

    angular js animation and spa animation and multiple view

--- feature/spa/uirouterconfg.js
+++ feature/spa/uirouterconfg.js
@@ -9,15 +9,27 @@ app.config(function($stateProvider, $urlRouterProvider){
                                     templateUrl:'/spatemplate/home.html',
                                     controller:'homeController'
                                 })
-                    .state("about",{
-                                        url:'/about',
-                                        template:'<h1>{{companyname}}: This is about us!</h1>',
-                                        controller:'aboutController'
-                                    })
-                    .state("contact",{
-                                        url:'/contact',
-                                        templateUrl:'/spatemplate/contact.html'
-                                    })
+        .state("about",{
+                            url:'/about',
+                            template:`<h1>{{companyname}}: This is about us!</h1>
+                                        <br> 
+                                        Child view: <a ui-sref=".param1">Param1</a>
+                                        <div class="page-container">
+                                            <div  class="page-view" ui-view></div>
+                                        </div>
+
+                                    `,
+                            controller:'aboutController'
+                        })
+        .state("about.param1",{
+                            url:"/param1",
+                            template:'<h1>{{param1}}: This is branch!</h1>',
+                            controller:'aboutChildController'
+                        })
+        .state("contact",{
+                            url:'/contact',
+                            templateUrl:'/spatemplate/contact.html'
+                        })
 
 
 
@@ -32,4 +44,10 @@ app.controller("homeController", function($scope){
 app.controller("aboutController", function($scope){
     $scope.companyname = "Cognizant"
     //$scope.childurl=$routeParams.childroute
+   
+})
+
+app.controller("aboutChildController", function($scope, $stateParams){
+    $scope.param1 = $stateParams.param1
+    console.log($stateParams.param1)
 })
